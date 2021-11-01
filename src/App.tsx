318bbe8f8f21e05fb4fe444473e4ac1e55e6b3b4
Bibiana.Sebestianova <17590227+Sebi2909@users.noreply.github.com>
2021-11-01 16:50:45 +0100

    react-pattern-19: Routing - basic routing with navigation - styling has to be improved as another issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { useTheme } from "../src/hooks/theme/themes-hook";
 import "./App.scss";
+import AsyncAwait from "./pages/AsyncAwait";
 import Home from "./pages/Home";
 import { GlobalStyles } from "./styles/styled-components/global-styles";
 
@@ -18,10 +20,28 @@ function App() {
       {themeLoaded && (
         <ThemeProvider theme={selectedTheme}>
           <GlobalStyles />
-          <div className="App">
-            <h1>React Patterns</h1>
-            <Home></Home>
-          </div>
+          <h1>React Patterns</h1>
+          <Router>
+            <nav className="main-nav">
+              <ol className="main-nav__list">
+                <li className="main-nav__list-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="main-nav__list-item">
+                  <Link to="/async">AsyncAwait</Link>
+                </li>
+              </ol>
+            </nav>
+
+            <Switch>
+              <Route path="/async">
+                <AsyncAwait />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
         </ThemeProvider>
       )}
     </>
